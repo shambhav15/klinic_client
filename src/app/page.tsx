ba164c0ed315144b81c085b8643d6f51504845a4
shambhav <shambhavse15@gmail.com>
2024-08-26 16:53:29 +0530

    initial css changes and added iamges and svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-export default function Home() {
+import RegisterForm from "@/components/form/RegisterForm";
+import Image from "next/image";
+import Link from "next/link";
+
+const Home = () => {
+
   return (
-    <main></main>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar relative flex-1 overflow-y-auto px-[5%] my-auto">
+        <div className="mx-auto flex size-full flex-col py-10 max-w-[496px]">
+          <div className="flex gap-4 mb-12 items-center">
+            <Image
+              src="/assets/icons/klinic.svg"
+              alt="Logo"
+              width={496}
+              height={496}
+              className="w-fit h-10"
+            />
+            <p className="text-white text-3xl font-semibold">Klinic</p>
+          </div>
+          <RegisterForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/register-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
-}
+};
+
+export default Home;
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -11,11 +11,11 @@ export default function ThemeToggle() {
             <Sun
 
                 size={24}
-                className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                className="absolute rotate-90 scale-0 transition-all duration-300 dark:rotate-0 dark:scale-100"
             />
             <Moon
                 size={24}
-                className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                className="rotate-0 scale-100 transition-all duration-300 dark:-rotate-90 dark:scale-0"
             />
         </button>
     );
